3778203b7466b17028b929bb4c4049b375529432
Mihail <lapanik.91@mail.ru>
2020-12-06 23:58:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+String str1 = " Hello";
+String str2 = " World";
+String str3 = "";
+for (int i=0;i<10;i++){
+     str3 += (str1+str2);
+    System.out.println(str3);
+}
+    }
+}
